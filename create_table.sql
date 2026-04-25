f94375b0a99d0693f189b792ce240d83773f21b9
Ngoc Lan Thanh Nguyen <63060935+thanh-nguyenn@users.noreply.github.com>
2026-04-25 14:14:02 +0300

    create index

--- create_table.sql
+++ create_table.sql
@@ -2,23 +2,27 @@ CREATE table Client (
   client_number INT PRIMARY KEY, 
   phone_number VARCHAR(10) NOT NULL
 );
+
 CREATE TABLE Venue (
   venue_number INT primary key, 
   venue_name varchar(50) not NULL, 
   address varchar(50) not NULL, 
   capacity INT not NULL
 );
+
 CREATE TABLE Artist (
   artist_number INT primary key, 
   artist_name varchar(50) not NULL, 
   contact_info varchar(50)
 );
+
 CREATE TABLE SpecificRequest (
   request_number INT primary key, 
   request_description VARCHAR(50), 
   artist_number int not NULL,
   CONSTRAINT SpecificRequest_Artist FOREIGN KEY (artist_number) REFERENCES Artist (artist_number)
 );
+
 CREATE TABLE Event (
   event_number INT primary key, 
   event_name VARCHAR(50) NOT NULL, 
@@ -30,6 +34,8 @@ CREATE TABLE Event (
   CONSTRAINT Event_Venue FOREIGN KEY (venue_number) REFERENCES Venue (venue_number)
 );
 
+CREATE INDEX IX_venue ON Event(event_number);
+
 CREATE TABLE Booking (
   booking_number INT primary key, 
   booking_date date NOT NULL, 
@@ -40,3 +46,7 @@ CREATE TABLE Booking (
   CONSTRAINT Booking_Client FOREIGN KEY (client_number) REFERENCES Client(client_number),
   CONSTRAINT Booking_Event FOREIGN KEY (event_number) REFERENCES Event(event_number)
 );
+
+CREATE INDEX IX_client_number ON Booking(client_number);
+CREATE INDEX IX_event_number ON Booking(event_number);
+
